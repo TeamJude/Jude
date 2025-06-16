8626fbaa8c9db70d0d41607fef714cc7104af9f8
d3ciee <decefemz@gmail.com>
2025-06-16 19:01:02 +0200

    feat: add rules models and migrations

---
+++ Jude.Server/Data/Models/RuleModel.cs
@@ -0,0 +1,22 @@
+namespace Jude.Server.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class RuleModel
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public RuleStatus Status { get; set; } = RuleStatus.Active;
+
+    [ForeignKey(nameof(UserModel))]
+    public Guid CreatedById { get; set; }
+}
+
+public enum RuleStatus
+{
+    Active, Inactive
+}
--- Jude.Server/Data/Repository/JudeDbContext.cs
+++ Jude.Server/Data/Repository/JudeDbContext.cs
@@ -18,4 +18,7 @@ public class JudeDbContext(DbContextOptions<JudeDbContext> options) : DbContext(
     public DbSet<UserModel> Users { get; set; }
 
     public DbSet<RoleModel> Roles { get; set; }
+    public DbSet<RuleModel> Rules { get; set; }
+
 }
+
